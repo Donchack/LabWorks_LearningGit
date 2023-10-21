cc9ed454b09255c837f3e582e481d3860f801b29
Donchack <flexway7@gmail.com>
2023-10-21 12:58:45 +0300

    Changes main.py
    To generate sequences, util.random_gen is used

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from random import randint
+from utils import random_gen
 
 
 user = "Alex"
@@ -6,5 +6,5 @@ print(f"Hello {user}!")
 rand_count = 10
 min_num = 0
 max_num = 100
-rand_seq = [randint(min_num, max_num) for i in range(rand_count)]
+rand_seq = random_gen.get_rand_int_seq(min_num, max_num, rand_count)
 print(f"This is random sequence: {rand_seq}")
