0c8136b810fc9bae7f60028ff8ac28cc371c64ee
Donchack <flexway7@gmail.com>
2023-10-21 12:40:53 +0300

    Changes main.py
    Added random sequence generation

--- app/main.py
+++ app/main.py
@@ -1,2 +1,10 @@
+from random import randint
+
+
 user = "Alex"
 print(f"Hello {user}!")
+rand_count = 10
+min_num = 0
+max_num = 100
+rand_seq = [randint(min_num, max_num) for i in range(rand_count)]
+print(f"This is random sequence: {rand_seq}")
